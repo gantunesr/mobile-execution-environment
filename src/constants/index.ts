faa101fdf37e7963a3b3ba0e1ebe6ba83bf9d938
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-12-15 15:13:59 -0300

    Update constants

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+import { IFRAME_URL } from './worker';
+import { METHODS } from './methods';
+
+export { IFRAME_URL, METHODS };
---
+++ src/constants/methods.ts
@@ -0,0 +1,6 @@
+export const METHODS = {
+  PING: 'ping',
+  EXECUTE_SNAP: 'executeSnap',
+  SNAP_RPC: 'snapRpc',
+  TERMINATE: 'terminate',
+}
